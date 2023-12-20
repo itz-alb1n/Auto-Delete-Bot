53f630cb52ffa76e34f185d88735254d45f13fce
itz-alb1n <115361467+itz-alb1n@users.noreply.github.com>
2023-12-20 22:59:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from pyrogram import Client, filters, idle
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
-SESSION = environ.get("SESSION")
 TIME = int(environ.get("TIME"))
 GROUPS = []
 for grp in environ.get("GROUPS").split():
@@ -15,14 +14,6 @@ for usr in environ.get("ADMINS").split():
     ADMINS.append(int(usr))
 
 START_MSG = "<b>Hai {},\nI'm a private bot of @mh_world to delete group messages after a specific time</b>"
-
-
-User = Client(name="user-account",
-              session_string=SESSION,
-              api_id=API_ID,
-              api_hash=API_HASH,
-              workers=300
-              )
 
 
 Bot = Client(name="auto-delete",
@@ -37,25 +28,24 @@ Bot = Client(name="auto-delete",
 async def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
 
-@User.on_message(filters.chat(GROUPS))
+@Bot.on_message(filters.chat(GROUPS))
 async def delete(user, message):
     try:
        if message.from_user.id in ADMINS:
           return
+       elif message.reply_to_message:
+           if (message.reply_to_message.via_bot and message.reply_to_message.via_bot.id == channel_id) or (message.via_bot and message.via_bot.id == channel_id):
+               return 
        else:
           await asyncio.sleep(TIME)
           await Bot.delete_messages(message.chat.id, message.id)
     except Exception as e:
        print(e)
        
-User.start()
-print("User Started!")
 Bot.start()
 print("Bot Started!")
 
 idle()
 
-User.stop()
-print("User Stopped!")
 Bot.stop()
 print("Bot Stopped!")
